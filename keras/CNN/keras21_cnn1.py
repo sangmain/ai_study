ab3e29c8569b1e8b19468af26e25f8b913fe84a8
sangmin99554 <sangmin99554@naver.com>
2019-07-30 09:08:00 +0900

    Update keras21_cnn1.py

--- keras/CNN/keras21_cnn1.py
+++ keras/CNN/keras21_cnn1.py
@@ -4,16 +4,20 @@ filter_size = 32
 kernel_size = (3,3)
 
 
-from keras.layers import Conv2D, MaxPooling2D, Flatten
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout
 model = Sequential()
-# model.add(Conv2D(7, (2,2), padding='same', input_shape=(5,5,1)))
-model.add(Conv2D(7, (2,2), padding='same', input_shape=(10,10,1)))
-# model.add(Conv2D(16,(2,2)))
-model.add(MaxPooling2D(3,3))
-# model.add(Conv2D(8,(2,2)))
-model.add(Dropout(0.2))
-## CNN의 마지막
-# model.add(Flatten())
-# model.add(Dense(30))
+
+
+model.add(Conv2D(3, (3,3), input_shape=(6,6,1)))
+
+# # model.add(Conv2D(7, (2,2), padding='same', input_shape=(5,5,1)))
+# model.add(Conv2D(7, (2,2), padding='same', input_shape=(10,10,1)))
+# # model.add(Conv2D(16,(2,2)))
+# model.add(MaxPooling2D(3,3))
+# # model.add(Conv2D(8,(2,2)))
+# model.add(Dropout(0.2))
+# ## CNN의 마지막
+# # model.add(Flatten())
+# # model.add(Dense(30))
 
 model.summary()
